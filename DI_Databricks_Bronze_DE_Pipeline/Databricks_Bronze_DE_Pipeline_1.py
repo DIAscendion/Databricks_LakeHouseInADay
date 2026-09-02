44ec66cc81e37ff5ed212388a9fff0739cf342cf
Suraj Naphade <suraj.naphade@ascendion.com>
2026-09-02 16:17:11 +0530

    Remove obsolete Databricks artifacts

--- DI_Databricks_Bronze_DE_Pipeline/Databricks_Bronze_DE_Pipeline_1.py
+++
@@ -1,4 +0,0 @@
-# _____________________________________________
-# *Author*: AAVA
-# *Created on*:   
-# *Description*:   PySpark pipeline to ingest raw data from source systems into the Databricks Bronze layer with comprehensive audit logging
